97b70ada3b30e86c5226f68c4e4acaef53e5b510
TARUNGORKA09 <tarungorka09@gmail.com>
2020-12-10 22:29:13 +0530

    sixth commit

--- HTTP_SERVER/HTTP_server.go
+++ HTTP_SERVER/HTTP_server.go
@@ -18,19 +18,19 @@ func main() {
 	sm := mux.NewRouter()
 	getRouter := sm.Methods(http.MethodGet).Subrouter()
 	getRouter.HandleFunc("/{id:[0-9]+}", prod.GetProducts)
-	getRouter.Use(prod.MiddlewareValidateproduct)
+	getRouter.Use(prod.MiddlewareValidateProduct)
 
 	putRouter := sm.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/{id:[0-9]+}", prod.UpdateProducts)
-	putRouter.Use(prod.MiddlewareValidateproduct)
+	putRouter.Use(prod.MiddlewareValidateProduct)
 
 	postRouter := sm.Methods(http.MethodPost).Subrouter()
 	postRouter.HandleFunc("/addproduct", prod.Addproducts)
-	postRouter.Use(prod.MiddlewareValidateproduct)
+	postRouter.Use(prod.MiddlewareValidateProduct)
 	s := &http.Server{
 		Addr:         ":8080",
 		Handler:      sm,
-		IdleTimeout :  120 * time.Second,	
+		IdleTimeout:  120 * time.Second,
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
--- HTTP_SERVER/handlers/product.go
+++ HTTP_SERVER/handlers/product.go
@@ -14,10 +14,11 @@ type Products struct {
 	l *log.Logger
 }
 
-type keyProduct struct
-
 func NewProduct(m *log.Logger) *Products {
 	return &Products{m}
+}
+
+type keyProduct struct {
 }
 
 func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
@@ -37,13 +38,9 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 func (p *Products) Addproducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Print("Update POST method")
 
-	prod := &data.Product{}
-	err := prod.ToDATA(r.Body)
-	if err != nil {
-		http.Error(rw, "unable to Convert ", http.StatusInternalServerError)
-	}
+	prod := r.Context().Value(keyProduct{}).(data.Product)
 	p.l.Print("Data %#v", prod)
-	data.AddProduct(prod)
+	data.AddProduct(&prod)
 }
 func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
@@ -52,22 +49,19 @@ func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
 		return
 	}
-	prod := r.Context().Value(KeyProduct{}).(data.Product)
-	err = data.UpdateProduct(id, prod)
+	prod := r.Context().Value(keyProduct{}).(data.Product)
+	err = data.UpdateProduct(id, &prod)
 }
 
-
-func (P Products) MiddlewareValidateProduct(next http.Handler) http.Handler{
-	return http.HandlerFunc(rw http.ResponseWriter, r *http.Request){
-		prod : = &data.Product{}
-		err1 := lp.ToDATA(r.Body)
+func (P Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+		err1 := prod.ToDATA(r.Body)
 		if err1 != nil {
 			http.Error(rw, "unable to Unmarshall JSON ", http.StatusInternalServerError)
-		}	
-		ctx := context.WithValue(r.Context(),keyProduct,prod)
+		}
+		ctx := context.WithValue(r.Context(), keyProduct{}, prod)
 		r = r.WithContext(ctx)
-		next.ServeHTTP(rw,r)
-	}
+		next.ServeHTTP(rw, r)
+	})
 }
-
-
